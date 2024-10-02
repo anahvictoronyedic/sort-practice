eec930c571b2edf574dac9e163c0cbecbd648abb
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-10-02 22:52:01 +0100

    Use snake case

--- bubble.py
+++ bubble.py
@@ -1,4 +1,4 @@
-def bubbleSort(arr):
+def bubble_sort(arr):
     n = len(arr)
     
     # Traverse through all array elements
@@ -8,7 +8,7 @@ def bubbleSort(arr):
         # Last i elements are already in place
         for j in range(0, n-i-1):
 
-            # Traverse the array from 0 to n-i-1
+            # Loop through the array from 0 to n-i-1
             # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
@@ -19,7 +19,7 @@ def bubbleSort(arr):
 if __name__ == "__main__":
     arr = [64,9, 90 43, 22, 23, 41, 91]
 
-    bubbleSort(arr)
+    bubble_sort(arr)
 
     print("Sorted array:")
     for i in range(len(arr)):
