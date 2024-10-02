a99154d63407b58ea58cd9cc6f0b17aae421e7c4
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-10-02 22:49:46 +0100

    Create selection.py

---
+++ selection.py
@@ -0,0 +1,34 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+      
+        # Assume the current position holds
+        # the minimum element
+        min_idx = i
+        
+        # Loop through the unsorted portion to find the actual minimum
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+              
+                # Update min_idx if a smaller element is found
+                min_idx = j
+        
+        # If a new minimum is found, swap it with the element at index i
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def print_array(arr):
+    for val in arr:
+        print(val, end=" ")
+    print()
+
+if __name__ == "__main__":
+    arr = [45 , 8 , 90 , 709 , 34]
+    
+    print("Input array: ", end="")
+    print_array(arr)
+    
+    selection_sort(arr)
+    
+    print("Sorted array: ", end="")
+    print_array(arr)
